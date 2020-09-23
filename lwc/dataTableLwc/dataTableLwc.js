993f7647e6f940c3872abc95fd8e69194424edac
MEA\Bikram.Behera <bikram.behera@gds.ey.com>
2020-09-23 22:47:06 +0530

    dataTable_firstCommit

---
+++ lwc/dataTableLwc/dataTableLwc.js
@@ -0,0 +1,26 @@
+import { LightningElement } from 'lwc';
+
+
+const columns = [
+    {label: 'Account name', fieldName: 'name', type: 'text'},
+    {label: 'Phone', fieldName: 'phone', type: 'percent', cellAttributes:
+    { iconName: { fieldName: 'trendIcon' }, iconPosition: 'right' }},
+    {label: 'Amount', fieldName: 'amount', type: 'currency', typeAttributes: { currencyCode: 'EUR'}},
+    {label: 'Contact Email', fieldName: 'contact', type: 'email'},
+    {label: 'Contact Phone', fieldName: 'phone', type: 'phone'},
+];
+
+
+export default class DataTableLwc extends LightningElement {
+
+    data = data;
+    columns = columns;
+
+    getSelectedName(event) {
+        const selectedRows = event.detail.selectedRows;
+        // Display that fieldName of the selected rows
+        for (let i = 0; i < selectedRows.length; i++){
+            alert("You selected: " + selectedRows[i].opportunityName);
+        }
+    }
+}
